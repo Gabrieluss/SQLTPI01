745a32448981836a686287a2cd6720a5ac9e4ada
Gabrieluss <gabrielduarte.sccp@gmail.com>
2023-10-20 11:51:12 -0300

    Criando sql

---
+++ dbLoterica.sql
@@ -0,0 +1,14 @@
+-- apagando o banco de dados
+drop database dbLoterica;
+
+-- criar o banco de dados
+create database dbLoterica;
+
+-- vizualisando o banco de dados
+show databases;
+
+-- acessando o banco de dados
+use dbLoterica;
+
+-- vizualsando as tabelas
+show tables;
